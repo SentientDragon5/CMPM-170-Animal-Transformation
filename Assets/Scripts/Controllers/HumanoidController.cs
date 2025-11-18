6515ca9d91b1656c0b3515c46c77920921ad87bd
Logan <34078184+SentientDragon5@users.noreply.github.com>
2025-11-18 14:45:11 -0800

    adding tags and grounding

--- Assets/Scripts/Controllers/HumanoidController.cs
+++ Assets/Scripts/Controllers/HumanoidController.cs
@@ -4,17 +4,40 @@ public class HumanoidController : GenericMoveController
 {    
     public float moveSpeed = 5;
     public float jumpSpeed = 10;
+    public LayerMask enviromentLayer = 128; // layer 7 so 2 ^ 7
 
     public override void Move(Vector3 moveInput)
     {
         // called on update, so we use this to move the player
         Vector3 groundMovement = new Vector3(moveInput.x, 0, moveInput.z);
-
-        rb.linearVelocity = groundMovement * moveSpeed;
+        
+        if (CheckGrounded(out Vector3 normal))
+        {
+            Vector3 move = groundMovement * moveSpeed;
+            rb.linearVelocity = Vector3.ProjectOnPlane(move, normal);
+        }
+        else
+        {
+            rb.linearVelocity += Physics.gravity;
+        }
     }
 
     public override void JumpAction()
     {
-        rb.linearVelocity += Vector3.up * jumpSpeed;
+        if (CheckGrounded(out Vector3 normal))
+        {
+            rb.linearVelocity += Vector3.up * jumpSpeed;
+        }
+    }
+
+    protected bool CheckGrounded(out Vector3 normal)
+    {
+        normal = Vector3.zero;
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 0.1f, enviromentLayer))
+        {
+            normal = hit.normal;
+            return true;
+        }
+        return false;
     }
 }
