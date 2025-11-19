809a5262b58668a1cb275d2b0360f21461cd79f8
Ayden Le <soloyeti41@gmail.com>
2025-11-18 17:22:45 -0800

    more experimentation

--- Assets/Scripts/Controllers/FishController.cs
+++ Assets/Scripts/Controllers/FishController.cs
@@ -5,17 +5,20 @@ public class FishController : GenericMoveController
     public float moveSpeed = 5;
     public float jumpSpeed = 10;
     public bool inWater = false;
-    public LayerMask enviromentLayer = 128; // layer 7 so 2 ^ 7
-
+    public LayerMask waterLayer = 32;
+    public LayerMask enviromentLayer = 128;
     public override void Move(Vector3 moveInput)
     {
         // called on update, so we use this to move the player
         Vector3 groundMovement = new Vector3(moveInput.x, 0, moveInput.z);
-        
-        if (CheckGrounded(out Vector3 normal))
+        if (CheckWater(out Vector3 wnormal))
         {
             Vector3 move = groundMovement * moveSpeed;
-            rb.linearVelocity = Vector3.ProjectOnPlane(move, normal);
+            rb.linearVelocity = Vector3.ProjectOnPlane(move, wnormal);
+        }
+        else if (CheckGrounded(out Vector3 normal))
+        {
+
         }
         else
         {
@@ -31,10 +34,17 @@ public class FishController : GenericMoveController
         }
     }
 
-    protected bool CheckWater()
+    protected bool CheckWater(out Vector3 normal)
     {
-
+        normal = Vector3.zero;
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 0.1f, waterLayer))
+        {
+            normal = hit.normal;
+            return true;
+        }
+        return false;
     }
+
 
     protected bool CheckGrounded(out Vector3 normal)
     {
